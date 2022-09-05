4b16e1fe972faa7d0e981c443d7d8ce4c6e077f2
RobertoDSouza <101618574+RobertoDSouza@users.noreply.github.com>
2022-09-05 09:30:51 -0300

    Finish
    last up css
    up php

---
+++ email.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['email']) && !empty($_POST['nome'])){
+
+$nome=addcslashes($_POST['nome'])
+$telefone=addcslashes($_POST['fone'])
+$mail=addcslashes($_POST['email'])
+$servico =addcslashes($_POST['select'])
+
+$to = "emailpara onde a msg vai@tal.com";
+$subject = "contato  MBRITO";
+$body = "Nome : ".$nome "\r\n".
+        "Email: " .$email. "\r\n".
+        "Telefone: ".$fone. "\r\n".
+        "Serviço: " .$select."\r\n";
+
+$header = "From:emaildodominio@gmail.com"."\r\n".
+        "Reply-To:".$email. "\r\n".
+        "X=Mailer:PHP/".phpversion();
+
+
+
+if(mail($to,$subject,$body,$header)){
+    echo("Email Enviado com Sucesso!");
+}
+ else{
+    echo("Email não pode ser enviado.");
+}
+
+}
+
+
+?>
